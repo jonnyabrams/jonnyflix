6fa4c29348b91c1fa1747df65fc30ce3a91af1af
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-16 00:54:36 +0100

    feat: root route is protected

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const Home = () => {
+  const session = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session.status === "unauthenticated") {
+      router?.push("/auth");
+    }
+  }, [session.status, router]);
+
+  if (session.status === "loading") {
+    return <p>Loading...</p>;
+  }
+
   const handleLogout = (event: any) => {
     event.preventDefault();
     signOut();
