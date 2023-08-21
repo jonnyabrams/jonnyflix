887b6c389f8856851dc9f9ffd0644c66a39043d4
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-21 08:04:21 +0100

    feat: add title and desc to billboard

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -6,14 +6,21 @@ const Billboard = () => {
   console.log(data?.videoUrl);
 
   return (
-    <div className="relative h-[56.26vw]">
+    <div className="relative h-[56.25vw]">
       <video
+        className="w-full h-[56.25vw] object-cover brightness-[60%]"
         autoPlay
         muted
         loop
         poster={data?.thumbnailUrl}
         src={data?.videoUrl}
-      ></video>
+      />
+      <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
+        <p className="text-white text-xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl">
+          {data?.title}
+        </p>
+        <p className="text-white text-[12px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl">{data?.description}</p>
+      </div>
     </div>
     // <div style="width:100%;height:0px;position:relative;padding-bottom:62.500%;"><iframe src="https://streamable.com/e/4bp51j" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
   );
