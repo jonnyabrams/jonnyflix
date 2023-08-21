39e1431879a950ed6a19bf6e06cc309139310724
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-21 10:10:24 +0100

    feat: info icon on Billboard btn

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,4 +1,5 @@
 import useBillboard from "@/hooks/useBillboard";
+import {AiOutlineInfoCircle} from 'react-icons/ai'
 
 const Billboard = () => {
   const { data } = useBillboard();
@@ -19,8 +20,15 @@ const Billboard = () => {
         <p className="text-white text-xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl">
           {data?.title}
         </p>
-        <p className="text-white text-[12px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl">{data?.description}</p>
-        <div className="flex items-center mt-3 md:mt-4 gap-3"><button className="bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold hover:bg-opacity-20 transition">More Info</button></div>
+        <p className="text-white text-[12px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl">
+          {data?.description}
+        </p>
+        <div className="flex items-center mt-3 md:mt-4 gap-3">
+          <button className="flex items-center bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold gap-1 hover:bg-opacity-20 transition">
+            <AiOutlineInfoCircle />
+            More Info
+          </button>
+        </div>
       </div>
     </div>
     // <div style="width:100%;height:0px;position:relative;padding-bottom:62.500%;"><iframe src="https://streamable.com/e/4bp51j" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
