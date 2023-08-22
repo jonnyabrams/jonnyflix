76d07c06276e78bd39c5efb475ee30bdac6bea27
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-22 15:16:27 +0100

    feat: all categories

--- app/page.tsx
+++ app/page.tsx
@@ -23,8 +23,23 @@ const Home = () => {
       <Navbar />
       <Billboard />
       <div className="pb-40">
-        <MovieList title="Trending Now" data={videos} />
-        <MovieList title="Trending Now" data={videos} />
+        <MovieList title="All" data={videos} />
+        <MovieList
+          title="React Projects"
+          data={videos.filter((video: any) => video.genre === "React")}
+        />
+        <MovieList
+          title="Next.js Projects"
+          data={videos.filter((video: any) => video.genre === "Next.js")}
+        />
+        <MovieList
+          title="React Native Projects"
+          data={videos.filter((video: any) => video.genre === "React Native")}
+        />
+        <MovieList
+          title="Ruby on Rails Projects"
+          data={videos.filter((video: any) => video.genre === "Ruby on Rails")}
+        />
       </div>
     </>
   );
