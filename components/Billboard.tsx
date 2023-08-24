8bd1f3f91473b279863c41fdd07e2a8e4136e1c0
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-24 10:28:46 +0100

    feat: PlayButton component + links

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,5 +1,7 @@
-import useBillboard from "@/hooks/useBillboard";
 import {AiOutlineInfoCircle} from 'react-icons/ai'
+
+import useBillboard from "@/hooks/useBillboard";
+import PlayButton from "./PlayButton";
 
 const Billboard = () => {
   const { data } = useBillboard();
@@ -24,6 +26,7 @@ const Billboard = () => {
           {data?.description}
         </p>
         <div className="flex items-center mt-3 md:mt-4 gap-3">
+          <PlayButton movieId={data?._id} />
           <button className="flex items-center bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold gap-1 hover:bg-opacity-20 transition">
             <AiOutlineInfoCircle />
             More Info
--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import { BsFillPlayFill } from "react-icons/bs";
+import { useRouter } from "next/navigation";
 
 interface MovieCardProps {
   data: Record<string, any>;
 }
 
 const MovieCard = ({ data }: MovieCardProps) => {
+  const router = useRouter();
+
   return (
-    <div className="bg-zinc-900 col-span relative h-[12vh]">
+    <div
+      onClick={() => router.push(`/watch/${data._id}`)}
+      className="bg-zinc-900 col-span relative h-[12vh]"
+    >
       <Image
         src={data.thumbnailUrl}
         fill={true}
@@ -15,19 +21,20 @@ const MovieCard = ({ data }: MovieCardProps) => {
         alt="thumbnail"
       />
 
-      <div className="flex flex-row items-center gap-3">
-        <div
-          onClick={() => {}}
-          className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white pl-0.5 rounded-full flex justify-center items-center transition hover:bg-neutral-300"
-        >
-          <BsFillPlayFill size={20} />
+      <div className="p-0.5">
+        <div className="flex flex-row items-center gap-3">
+          <div className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white pl-0.5 rounded-full flex justify-center items-center transition hover:bg-neutral-300">
+            <BsFillPlayFill size={20} />
+          </div>
         </div>
-      </div>
 
-      <p className="text-white text-[12px] font-semibold mt-4">{data.title}</p>
+        <p className="text-white text-[12px] font-semibold mt-4 line-clamp-1">
+          {data.title}
+        </p>
 
-      <div className="flex mt-2 gap-2 items-center">
-        <p className="text-white text-[10px]">{data.genre}</p>
+        <div className="flex mt-2 gap-2 items-center">
+          <p className="text-white text-[10px]">{data.genre}</p>
+        </div>
       </div>
     </div>
   );
