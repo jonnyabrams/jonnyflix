359620ac216ce6cdf503d05d92d398a8af9c1fb1
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-24 11:22:45 +0100

    feat: useInfoModal zustand store

---
+++ hooks/useInfoModal.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+export interface ModalStoreInterface {
+  videoId?: string;
+  isOpen: boolean;
+  openModal: (videoId: string) => void;
+  closeModal: () => void;
+}
+
+const useInfoModal = create<ModalStoreInterface>((set) => ({
+  videoId: undefined,
+  isOpen: false,
+  openModal: (videoId: string) => set({ isOpen: true, videoId }),
+  closeModal: () => set({ isOpen: false, videoId: undefined }),
+}));
+
+export default useInfoModal;
